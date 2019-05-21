301f0158757205ae8ad26b06d525800034b67a3e
minwiki7456 <minwiki7456@163.com>
2019-05-21 12:39:55 +0800

    2nd problem's java code.

--- TwoSum/Minwiki7456.java
+++ TwoSum/Minwiki7456.java
@@ -1,28 +1,27 @@
-ï»¿import java.util.*;
-
-public class Minwiki7456{
+import java.util.Scanner;
+public class Minwiki7456
+{
 	public static void main(String[] args)
 	{
 		int[] mylist=new int[10];
 		int target=0;
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Please enter 10 integersï¼š");
+		System.out.println("Please enter 10 integers£º");
 		for(int i=0;i<10;i++)
 		{
 			mylist[i]=scan.nextInt();
 		}
-		System.out.println("Please enter a target valueï¼š");
+		System.out.println("Please enter a target value£º");
 		target=scan.nextInt();
 		for(int i=0;i<9;i++)
-		{
-			for(int j=i+1;j<10;j++)
 			{
+			for(int j=i+1;j<10;j++)
+				{
 				if((mylist[i]+mylist[j])==target)
 					{
 						System.out.println("["+i+","+j+"]");
 					}
+				}
 			}
 		}
-		
-	}
 }
