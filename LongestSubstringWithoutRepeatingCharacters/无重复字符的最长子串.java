a23528dfb4f4035ac53caa864634c8482458ba6a
foolishPig <1315002130@qq.com>
2019-05-23 15:05:52 +0800

    交作业

---
+++ LongestSubstringWithoutRepeatingCharacters/无重复字符的最长子串.java
@@ -0,0 +1,25 @@
+package com.LeetCode;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class 无重复字符的最长子串 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str = s.next();
+		int length = 0,left = 0,right = 0;
+		HashSet hs = new HashSet();
+		while(right<str.length()) {
+			if(!hs.contains(str.charAt(right))) {
+				hs.add(str.charAt(right));
+				right++;
+				length = Math.max(length,hs.size());
+			}else {
+				hs.remove(str.charAt(left));
+				left++;
+			}
+		}
+		System.out.println(length);
+	}
+}
