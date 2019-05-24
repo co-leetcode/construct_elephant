2edc588b6dd1c7996eba72a405979befe732d88d
foolishPig <1315002130@qq.com>
2019-05-24 23:51:29 +0800

    Version2.0

--- MedianofTwoSortedArrays/foolishPig.java
+++ MedianofTwoSortedArrays/foolishPig.java
@@ -1,59 +1,56 @@
-package com.LeetCode;
+package 牛客网调试器;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+public class foolishPig {
 
-public class 寻找两个有序数组的中位数 {	
-	static Scanner s = new Scanner(System.in);
-	public static void main(String args[]) {
-		double binary = 0;		//中位数
-		System.out.println("请输入数组a大小及数值!");
-		System.out.println("数组大小:");
-		int m = s.nextInt();
-		System.out.println("数组数值:");
-		int[] a = constructArr(m);		//将数组初始化
-		System.out.println("请输入数组b大小及数值!");
-		System.out.println("数组大小:");
-		int n = s.nextInt();
-		System.out.println("数组数值:");
-		int[] b = constructArr(n);
-		ArrayList<?> als = new ArrayList<Object>();
-		als = addArr(a,b);		//将数组元素存进集合
-		Object[] c = als.toArray();
-		Arrays.sort( c);		//对集合内元素进行排序
-		if(c.length%2==1) {
-			binary = (int) c[c.length/2];
-		}else {
-			int a1=0,b1=0;
-			a1 = (int) c[c.length/2-1];
-			b1 = (int) c[c.length/2];
-			binary = (a1+b1)/2.0;
-		}
-		System.out.println(binary);
+	public static void main(String[] args) {
+		int a[] = {1,2,3,4,5,6,7,8,9};
+		int b[] = {1,2,3,4,5,6};
+		System.out.println(find(a,b));
 	}
-	
-	//将数组元素加入ArrayList集合中
-	public static ArrayList <Integer>  addArr(int[] a,int[] b) {
-		ArrayList <Integer> al = new ArrayList<Integer>();
-		int i = 0,j = 0;
-		while(i<a.length) {
-			al.add(a[i]);
-			i++;
-		}
-		while(j<b.length) {
-			al.add(b[j]);
-			j++;
-		}
-		return al;
+	//返回较大数字
+	public static double returnMax(double a,double b) {
+		if(a>b)
+			return a;
+		else
+			return b;
 	}
-	
-	//循环初始化数组
-	public static int[] constructArr(int x) {
-		int[] arr = new int[x];
-		for(int i=0;i<x;i++) {
-			arr[i] = s.nextInt();
+	//返回较小数字
+	public static double returnMin(double a,double b) {
+		if(a<b)
+			return a;
+		else
+			return b;
+	}
+	//查找中位数
+	public static double find(int[] a,int[] b) {
+		if(a.length>b.length)
+			return find(b,a);		//保证是在长度小的数组中查找切割的合适位置
+		int length = a.length+b.length;
+		int cut1 = 0;		//数组1切割的合适位置
+		int cut2 = 0;		//数组2切割的合适位置
+		int cutL = 0;  		//开始切割的变化范围
+		int cutR = a.length;
+		while(cut1<=a.length) {
+			cut1 = (cutR-cutL)/2+cutL;		//二分查找更新每次切割的位置
+			cut2 = (length/2)-cut1;
+			//初始化切割后所查找到的中位数
+			double L1 = a[cut1-1];
+			double L2 = b[cut2-1];
+			double R1 = a[cut1];
+			double R2 = b[cut2];
+			//保证切割后两数组之间任意左边的元素都小于任意右边的元素,且使左右两边元素数目尽可能相同
+			if(L1>R2) 
+				cutR = cut1-1;
+			else if(L2>R1)
+				cutL = cut1+1;
+			else {
+				if(length%2==0) {		//若两数组长度和为偶数,则中位数为中间两数的平均数
+					return (returnMax(L1,L2)+returnMin(R1,R2));
+				}else {								//若两数组长度和为奇数,则中位数为最中间的数字
+					return returnMin(R1,R2);		
+				}
+			}
 		}
-		return arr;
+		return -1;
 	}
 }
