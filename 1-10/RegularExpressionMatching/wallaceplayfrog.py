9a405b8546ef23bbda31d221f6ce6085f308f92e
wallacepalyfrog <2425779559@qq.com>
2019-05-30 15:00:40 +0800

    flag已立好

--- 1-10/RegularExpressionMatching/wallaceplayfrog.py
+++ 1-10/RegularExpressionMatching/wallaceplayfrog.py
@@ -1,3 +1,28 @@
 class Solution(object):
     def isMatching(self, s, p):
-        pass
+        #if not s or not p:
+            #return False
+        s_len = len(s)
+        p_len = len(p)
+        dp = [[False] * (p_len + 1) for _ in range(s_len + 1)]
+        print(dp)
+        dp[0][0] = True
+        for i in range(p_len):
+            if p[i] == "*" and dp[0][i - 1]:
+                dp[0][i + 1] = True
+        print(dp)
+        for i in range(s_len):
+            for j in range(p_len):
+                if p[j] == s[i] or p[j] == ".":
+                    dp[i + 1][j + 1] = dp[i][j]
+                elif p[j] == "*":
+                    if p[j - 1] != s[i]:
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1]
+                    if p[j-1] == s[i] or p[j-1] == ".":
+                        dp[i+1][j+1] = (dp[i][j+1] or dp[i+1][j]   or  dp[i+1][j-1])
+        print(dp)
+        return dp[-1][-1]
+
+if __name__ == "__main__":
+    solve = Solution()
+    print(solve.isMatching('aa', 'a'))
