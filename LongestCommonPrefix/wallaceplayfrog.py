94a4ca63582f06fd52b1b477d790ae0e8a5b5ec8
wallaceplayfrog <2425779559@qq.com>
2019-06-03 23:45:07 +0800

    jiaozuoye

---
+++ LongestCommonPrefix/wallaceplayfrog.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs: return ""
+        ss = list(map(set, zip(*strs)))
+        res = ""
+        for i, x in enumerate(ss):
+            x = list(x)
+            if len(x) > 1:
+                break
+            res = res + x[0]
+        return res
+
+if __name__ == "__main__":
+    solve = Solution()
+    print(solve.longestCommonPrefix(["flower","flow","flight"]))
