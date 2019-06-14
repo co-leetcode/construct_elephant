d313cf1b38b65d825c0ee181221dfeb1aed36f04
wallaceplayfrog <2425779559@qq.com>
2019-06-14 21:26:46 +0800

    递归

---
+++ SwapNodesInPairs/wallaceplayfrog.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        nextnode = head.next
+        head.next = self.swapPairs(nextnode.next)
+        nextnode.next = head
+        return nextnode
