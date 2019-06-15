b97b9b275e664641c3e7beef255dda6d9f84183c
wallaceplayfrog <2425779559@qq.com>
2019-06-15 13:25:14 +0800

    two pointers

---
+++ RemoveDuplicatesFromSortedArray/wallaceplayfrog.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) <= 1:
+            return len(nums)
+        i, j = 0, 1
+        while j < len(nums):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+            j += 1
+        return i+1
+
+solve = Solution()
+print(solve.removeDuplicates([1,2,3,4,5,5,5,6]))
