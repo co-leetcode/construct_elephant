174ae6ed9d04a26a65b1fc326f661a799ce069b0
wallaceplayfrog <2425779559@qq.com>
2019-06-17 10:58:52 +0800

    find

---
+++ ImplementStrstr/wallaceplayfrog.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle == '':
+            return 0
+        for i in range(0, len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
