69840c12dc871cda276ca176f9cc3fd503be5a8a
AJ宝宝 <18329349670@163.com>
2019-06-17 23:58:17 +0800

    aj

---
+++ ImplementStrstr/aj'10.py
@@ -0,0 +1,20 @@
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+
+        if haystack == needle:
+            return 0
+
+        i = 0
+        needle_len = len(needle)
+        while i + needle_len <= len(haystack):
+            if haystack[i:i + needle_len] == needle:
+                return i
+            else:
+                i = i + 1
+
+        return -1
