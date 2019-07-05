475e7ac5ca428ba1f8a306a5758d86d611bb7163
wallaceplayfrog <2425779559@qq.com>
2019-07-05 11:46:41 +0800

    第一遍题没读懂2333

--- CountAndSay/wallaceplayfrog.py
+++ CountAndSay/wallaceplayfrog.py
@@ -5,20 +5,29 @@ class Solution(object):
         :rtype: str
         """
         count = 1
+        r = '1'
         res = ''
-        i = 0
-        while i < len(n) - 1:
-            if n[i] == n[i+1]:
-                count += 1
-                i += 1
-                if i >= len(n) - 1:
-                    res += str(count) + str(n[i])
+        k = 1
+        while k < n:
+            t = r
+            i = 0
+            while i < len(t):
+                if i + 1 >= len(t):
+                    res += str(count) + str(t[i])
+                    count = 1
                     break
-            else:
-                res += str(count) + str(n[i])
-                count = 1
-                i += 1
-        return res
+                if t[i] == t[i + 1]:
+                    count += 1
+                    i += 1
+                else:
+                    res += str(count) + str(t[i])
+                    count = 1
+                    i += 1
+            r = res
+            res = ''
+            k += 1
+        return r
 
 solve = Solution()
-print(solve.countAndSay('1211'))
+for i in range (1, 10):
+    print(solve.countAndSay(i))
