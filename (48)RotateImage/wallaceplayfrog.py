82743d0c1df03308adb6c6f45b316a27ee1bde57
wallaceplayfrog <2425779559@qq.com>
2019-07-22 16:58:31 +0800

    read

---
+++ (48)RotateImage/wallaceplayfrog.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix[0])
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                tmp = [0] * 4
+                row, col = i, j
+                # store 4 elements in tmp
+                for k in range(4):
+                    tmp[k] = matrix[row][col]
+                    row, col = col, n - 1 - row
+                # rotate 4 elements   
+                for k in range(4):
+                    matrix[row][col] = tmp[(k - 1) % 4]
+                    row, col = col, n - 1 - row
