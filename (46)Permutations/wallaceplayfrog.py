7e83fec9ff9a76fc4b89f4d0b376a21499211889
wallaceplayfrog <2425779559@qq.com>
2019-07-25 15:47:10 +0800

    read

---
+++ (46)Permutations/wallaceplayfrog.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        def backtrack(nums, tmp):
+            if not nums:
+                res.append(tmp)
+                return
+            for i in range(len(nums)):
+                backtrack(nums[:i] + nums[i+1:], tmp + [nums[i]])
+        backtrack(nums, [])
+        return res
