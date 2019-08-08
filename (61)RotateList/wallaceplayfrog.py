a37586fa62042ced56a2d3144f96db09a0d7a4d7
wallaceplayfrog <2425779559@qq.com>
2019-08-08 15:42:21 +0800

    链表成环

--- (61)RotateList/wallaceplayfrog.py
+++ (61)RotateList/wallaceplayfrog.py
@@ -20,8 +20,7 @@ class Solution(object):
             count += 1
             stack.append(p.val)
             p = p.next
-        if k > count:
-            k = k % count
+        k = k % count
         while k:
             stack.insert(0, stack.pop())
             k -= 1
@@ -30,3 +29,25 @@ class Solution(object):
             q.next = ListNode(stack.pop(0))
             q = q.next
         return p.next
+
+    def _rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        if head is None or head.next is None:
+            return head
+        p = q = head
+        count = 1
+        while q.next:
+            count += 1
+            q = q.next
+        k = count - (k % count)
+        q.next = p
+        while k:
+            p = p.next
+            q = q.next
+            k -= 1
+        q.next = None
+        return p
